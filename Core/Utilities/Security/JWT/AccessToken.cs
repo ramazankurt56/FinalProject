77b5fb4e2ced07c3fa1a0e406d52b564e3217d0d
KURT <KURT@KURT>
2023-06-02 20:41:12 +0300

    2.06.2023-20:40

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken
+    {
+        public int Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
